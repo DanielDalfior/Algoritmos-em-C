4d3965fc03fc804ffd7c47abcb525fcfb181a22c
Daniel Dalfior <159311745+DanielDalfior@users.noreply.github.com>
2025-08-11 10:53:03 -0300

    Calculo de mercadoria
    Mercadoria com desconto, sem desconto e o desconto.

--- main.c
+++ main.c
@@ -1,31 +1,28 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main(void) {
-  int N1, N2;
+int main()
+{
+    float preco, reajuste;
+    float totalBruto, totalLiquido, desconto;
+    int quantidade;
 
-  printf("Mini calculadora de inteiros:\n");
-  printf("Valor de N1:\n");
-  scanf("%i", &N1);
-  printf("Valor de N2:\n");
-  scanf("%i", &N2);
-  printf("SAÍDA DAS OPERAÇÕES ARITMETICAS BASICAS:\n");
-  printf("ADIÇÃO: N1 + N2 = %i + %i = %i\n", N1, N2, N1 + N2);
-  printf("SUBTRAÇÃO: N1 - N2 = %i - %i = %i\n", N1, N2, N1 - N2);
-  printf("MULTIPLICAÇÃO: N1 * N2 = %i * %i = %i\n", N1, N2, N1 * N2);
-  printf("DIVISÃO COM VIRGULA: N1 / N2 = %i / %i = %.1f\n", N1, N2, (float) N1 / N2);  
-  printf("DIVISÃO INT: N1 / N2 = %i / %i = %i\n", N1, N2, N1 / N2);
-  printf("RESTO DA DIVISÃO: N1 % N2 = (%i) % (%i) = %i\n", N1, N2, N1 % N2);
-  printf("POTENCIALIZAÇÃO: N1 pow N2 = %i pow %i = %.1f\n", N1, N2, (float) pow(N1,N2));
-  printf("RADICIAÇÃO: N1 rad N2 = %i rad %i = %.1f\n", N1, N2, (float) pow(N1, 1.0 / N2));
-   return 0;
+    printf("MERCADORIA: Dados de entrada\n");
+    printf("Preco (R$): "); scanf("%f", &preco);
+    printf("Quantidade (Uni.): "); scanf("%i", &quantidade);
+    printf("Reajuste (%%): "); scanf("%f", &reajuste);
+
+    totalBruto = preco * quantidade;
+    desconto = reajuste / 100 * totalBruto;
+    totalLiquido = totalBruto - desconto;
+
+    printf("Total a pagar (sem desconto): R$ %.2f\n", totalBruto);
+    printf("Total a pagar (com desconto: %.2f %%): R$  %.2f\n", reajuste, totalLiquido);
+    printf("Desconto (%.2f %%): R$ %.2f\n", reajuste, desconto);
+
+
+
+
+    return 0;
 }
