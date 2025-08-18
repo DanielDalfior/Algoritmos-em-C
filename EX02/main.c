bfdaed67701e61220001eafde8c04eecac39ed56
Daniel Dalfior <159311745+DanielDalfior@users.noreply.github.com>
2025-08-18 10:21:52 -0300

    Senha de criptografia forte ou fraca
    Impar e multipla de 17: criptografia forte // else: criptografia fraca

--- EX02/main.c
+++ EX02/main.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
-    float preco, reajuste;
-    float totalBruto, totalLiquido, desconto;
-    int quantidade;
+    int senha;
+    printf("AUTENTIFICACAO:\n");
+    printf("Senha Numerica (XXXX): "); scanf("%i", &senha);
 
-    printf("MERCADORIA: Dados de entrada\n");
-    printf("Preco (R$): "); scanf("%f", &preco);
-    printf("Quantidade (Uni.): "); scanf("%i", &quantidade);
-    printf("Reajuste (%%): "); scanf("%f", &reajuste);
-
-    totalBruto = preco * quantidade;
-    desconto = reajuste / 100 * totalBruto;
-    totalLiquido = totalBruto - desconto;
-
-    printf("Total a pagar (sem desconto): R$ %.2f\n", totalBruto);
-    printf("Total a pagar (com desconto: %.2f %%): R$  %.2f\n", reajuste, totalLiquido);
-    printf("Desconto (%.2f %%): R$ %.2f\n", reajuste, desconto);
+    if(senha <= 999 || senha >= 10000)
+        printf("ERRO: Senha deve ter 4 digitos");
+    else{
+        if(senha % 2 != 0 && senha % 17 == 0)
+            printf("Senha de criptografia: Forte\n");
+        else
+            printf("Senha de criptografia: Fraca\n");
+    }
 
     return 0;
 }
