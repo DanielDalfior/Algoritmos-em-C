3fa30641efbc425f0b66b0c8542b8d1e5b393dd6
Daniel Dalfior <daniel.f.dalfior@gmail.com>
2025-11-18 20:02:19 -0300

    Update main.c

--- EX01/main.c
+++ EX01/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define PI 3.14
 
 int main()
 {
-    float preco, total;
-    int quantidade;
-    printf("CARRINHO DE COMPRA:\n");
-    printf("MERCADORIA: Dados de entrada:\n");
-    printf("Preco (R$): "); scanf("%f", &preco);
-    printf("Quantidade (Unid.): "); scanf("%i", &quantidade);
+    float R, area, volume, volumeLitros;
 
-    total = preco * quantidade;
-    if(total > 1000.00)
-        total *= 0.97;
-    printf("Total a pagar: R$ %.2f\n", total);
+    printf("ESFERA: Dados de entrada: \n");
+    printf("Valor do Raio (em metros): ");
+    scanf("%f", &R);
 
+    area = 4 * PI * pow(R, 2);
+    volume = 4.0 / 3 * PI * pow(R, 3);
+    volumeLitros = 1000 * volume;
 
+    printf("Dados da Esfera: \n");
+    printf("Area: %.2f Metros Quadrados\n", area);
+    printf("Volume: %.2f Metros Cubicos\n", volume);
+    printf("Volume: %.2f Litros\n", volumeLitros);
     return 0;
 }
